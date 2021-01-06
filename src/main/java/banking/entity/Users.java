91760c48895049f1c26f89eaa9fc93e821da1c99
Khanh269 <76761739+Khanh269@users.noreply.github.com>
2021-01-06 10:12:37 +0700

    Main

---
+++ src/main/java/banking/entity/Users.java
@@ -0,0 +1,38 @@
+package banking.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private long balance;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+
+}
